767ef344bd28c7d49f9bda8967539178833d0ee1
Svetlana Stoycheva <s.stoycheva@gmail.com>
2021-04-08 19:02:17 +0300

    second commit

---
+++ src/pages/index.js
@@ -0,0 +1,19 @@
+import Home from './HomePage'
+import About from './AboutPage'
+import PrivateRoute from './PrivateRoute'
+import Cart from './CartPage'
+import Checkout from './CheckoutPage'
+import Error from './ErrorPage'
+import Products from './ProductsPage'
+import SingleProduct from './SingleProductPage'
+
+export {
+  Home,
+  About,
+  PrivateRoute,
+  Cart,
+  Checkout,
+  Error,
+  Products,
+  SingleProduct,
+}
